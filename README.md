fc770373d5077580cd59e9f63d0c31ba576c5d03
cyberphinx <88593809+Cyberphinx@users.noreply.github.com>
2024-03-24 00:29:38 +0000

    :sparkles: added index.html

--- README.md
+++ README.md
@@ -11,5 +11,10 @@ To run:
 ```bash
 bun run index.ts
 ```
+or
+
+```bash
+bun run start
+```
 
 This project was created using `bun init` in bun v1.0.35. [Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
--- index.ts
+++ index.ts
@@ -1,1 +1,7 @@
-console.log("Hello via Bun!");
+const server = Bun.serve({
+  fetch(req: Request): Response | Promise<Response> {
+    return new Response(Bun.file("./index.html"));
+  },
+  port: process.env.PORT || 3000,
+});
+console.log(`Listening on http://localhost:${server.port} ...`);
